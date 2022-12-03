2befbb9015e79b58d76f2391d012b67613a72215
Stellareign <tuguran@mail.ru>
2022-12-03 18:12:27 +0300

    Билет 7, задача

---
+++ ticket 7/src/Service.java
@@ -0,0 +1,12 @@
+import java.util.Objects;
+public class Service {
+
+    public static double quadrat(double x) {
+        int j = 2;
+        double quadratNumber = x;
+        quadratNumber = Math.pow(quadratNumber, j);
+
+        System.out.println(x + " в степени " + j + " равно " + quadratNumber);
+        return quadratNumber;
+    }
+}
